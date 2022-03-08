2869235675674519f7f533c067a559244d40a6d4
Rishi Malani <malanirishi@gmail.com>
2022-03-07 22:33:12 -0800

    adding html reports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { WinsAnalysis } from './composition/analyzers/WinsAnalysis';
 import { CSVFileReader } from './composition/CSVFileReader';
 import { MatchReader } from './composition/MatchReader';
 import { ConsoleReports } from './composition/reportTargets/ConsoleReports';
+import { HtmlReport } from './composition/reportTargets/HtmlReports';
 import { Summary } from './composition/Summary';
 
 /* inheritance
@@ -11,5 +12,9 @@ reader.read(); */
 const csvFileReader = new CSVFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+/* using console report
 const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReports());
+summary.buildAndPrintReport(matchReader.matches); */
+
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
